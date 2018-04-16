705a738aa3b39bed6a7297aaeaeeae5851956d3e
Markandaiya <markandaiya.falconet@gmail.com>
2018-04-16 22:22:31 +0530

    Completing sign in process.

---
+++ dillyBagApp/Setup/SignedOutTeacherViewController.swift
@@ -0,0 +1,26 @@
+//
+//  SignedOutTeacherViewController.swift
+//  dillyBagApp
+//
+//  Created by Markandaiya Harsha on 16/04/18.
+//  Copyright © 2018 markandaiya. All rights reserved.
+//
+
+import UIKit
+
+class SignedOutTeacherViewController: GeneralViewController {
+
+   
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
