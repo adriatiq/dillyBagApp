8d9608118e4804a0f3fd1baa43af447cc554d275
Markandaiya <markandaiya.falconet@gmail.com>
2018-04-16 16:54:45 +0530

    SetUp Views, visitors direct adventure.

---
+++ dillyBagApp/UtilityClasses/GeneralViewController.swift
@@ -0,0 +1,43 @@
+//
+//  GeneralViewController.swift
+//  dillyBagApp
+//
+//  Created by Markandaiya Harsha on 16/04/18.
+//  Copyright © 2018 markandaiya. All rights reserved.
+//
+
+import UIKit
+
+class GeneralViewController: UIViewController {
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.view.setBackground()
+        self.view.subViewGayab()
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.view.fadeIn()
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.view.fadeOut()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
