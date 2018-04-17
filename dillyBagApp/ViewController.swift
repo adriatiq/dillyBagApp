fce3d8c48f6fb6132b8b44b4108d87a5bf7f3059
markandaiya89 <markandaiya.falconet@gmail.com>
2018-04-17 18:06:44 +0530

    Revert "Initial view"

--- dillyBagApp/ViewController.swift
+++ dillyBagApp/ViewController.swift
@@ -8,32 +8,18 @@
 
 import UIKit
 
-class ViewController: GeneralViewController {
+class ViewController: UIViewController {
 
-    @IBOutlet weak var loadingLabelWidth: NSLayoutConstraint!
-   
-
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        AnimateHeight()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
     }
-    
-  
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
 
-    func AnimateHeight() {
-        self.loadingLabelWidth.constant = 389
-        self.view.needsUpdateConstraints()
-        UIView.animate(withDuration: 1.0, animations: {
-            self.view.layoutIfNeeded()
-        }) { (isComplete) in
-        self.performSegue(withIdentifier: "toStart", sender: self)
-        }
-        
-    }
 }
 
